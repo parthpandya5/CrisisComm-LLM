a4a9848c8ea0784dada4bd14b295d2f2377407c2
parthpandya5 <143971991+parthpandya5@users.noreply.github.com>
2025-01-17 16:41:21 -0500

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -4,11 +4,7 @@ import firebase_admin
 from firebase_admin import credentials, auth, db
 
 # Check if Firebase app is already initialized
-if not firebase_admin._apps:
-    cred = credentials.Certificate('.gitignore')
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://crisiscomm-llm-default-rtdb.firebaseio.com/'
-    })
+if not firebase_admin._apps: cred = credentials.Certificate({ "type": st.secrets["firebase"]["type"], "project_id": st.secrets["firebase"]["project_id"], "private_key_id": st.secrets["firebase"]["private_key_id"], "private_key": st.secrets["firebase"]["private_key"], "client_email": st.secrets["firebase"]["client_email"], "client_id": st.secrets["firebase"]["client_id"], "auth_uri": st.secrets["firebase"]["auth_uri"], "token_uri": st.secrets["firebase"]["token_uri"], "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"], "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"], "universe_domain": st.secrets["firebase"]["universe_domain"] }) firebase_admin.initialize_app(cred, { 'databaseURL': 'https://crisiscomm-llm-default-rtdb.firebaseio.com/' })
 
 # Function to register user
 def register_user(email, password):
